75ac0b555713ad12242f79b382ad7c045f8ec058
a-jb97 <a_jb97@naver.com>
2026-01-27 15:41:49 +0900

    Fix : StarButton을 Extension에서 Class로 변경

--- PhotoProject/CustomView/StarButton.swift
+++ PhotoProject/CustomView/StarButton.swift
@@ -7,20 +7,26 @@
 
 import UIKit
 
-extension UIButton {
-    static let starButton = {
-        let button = UIButton()
+class StarButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
         
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 14
-        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
-        button.setImage(UIImage(systemName: "star.fill"), for: .normal)
-        button.setTitle("123,456", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 12)
-        button.tintColor = .systemYellow
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .darkGray
-        
-        return button
-    }()
+        clipsToBounds = true
+        layer.cornerRadius = 14
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        setImage(UIImage(systemName: "star.fill"), for: .normal)
+        setTitle("123,456", for: .normal)
+        titleLabel?.font = .systemFont(ofSize: 12)
+        tintColor = .systemYellow
+        setTitleColor(.white, for: .normal)
+        backgroundColor = .darkGray
+    }
 }
