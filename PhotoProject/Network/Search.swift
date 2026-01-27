0c5f05ec0919a09045bf94dbd30b8b576b6c0c7f
a-jb97 <a_jb97@naver.com>
2026-01-27 16:44:42 +0900

    Feat : Search Decodable 구조체 생성

---
+++ PhotoProject/Network/Search.swift
@@ -0,0 +1,22 @@
+//
+//  Search.swift
+//  PhotoProject
+//
+//  Created by 전민돌 on 1/27/26.
+//
+
+import Foundation
+
+struct Search: Decodable {
+    let results: [SearchDetail]
+}
+
+struct SearchDetail: Decodable {
+    let id: String
+    let created_at: String
+    let width: Int
+    let height: Int
+    let urls: [UrlsDetail]
+    let likes: Int
+    let user: [UserDetail]
+}
---
+++ PhotoProject/Network/SharedStruct.swift
@@ -0,0 +1,8 @@
+//
+//  SharedStruct.swift
+//  PhotoProject
+//
+//  Created by 전민돌 on 1/27/26.
+//
+
+import Foundation
