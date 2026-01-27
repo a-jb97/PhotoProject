e7c590c3655d27664487da25609eecacf1ef62b0
a-jb97 <a_jb97@naver.com>
2026-01-27 09:12:47 +0900

    Design : SearchPhotoVC에서 사용하는 FilterCollectionViewCell 설정

--- PhotoProject/Search/FilterCollectionViewCell.swift
+++ PhotoProject/Search/FilterCollectionViewCell.swift
@@ -6,7 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
-class FilterCollectionViewCell: UICollectionViewCell {
+class FilterCollectionViewCell: BaseCollectionViewCell {
+    let colorImageView = {
+        let imageView = UIImageView()
+        
+        imageView.backgroundColor = .black
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        
+        return imageView
+    }()
+    let colorLabel = {
+        let label = UILabel()
+        
+        label.text = "블랙"
+        
+        return label
+    }()
     
+    override func configureHierarchy() {
+        [colorImageView, colorLabel].forEach { contentView.addSubview($0) }
+    }
+    
+    override func configureLayout() {
+        colorImageView.snp.makeConstraints { make in
+            make.verticalEdges.equalTo(contentView).offset(4)
+            make.leading.equalTo(contentView).offset(4)
+            make.height.width.equalTo(20)
+        }
+        
+        colorLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(colorImageView)
+            make.leading.equalTo(colorImageView.snp.trailing).offset(4)
+        }
+    }
+    
+    override func configureView() {
+        contentView.backgroundColor = .lightGray
+    }
 }
