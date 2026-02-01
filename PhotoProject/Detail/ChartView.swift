f74a03cb7904825b1df6238ea4d9e9821bb1e2b2
a-jb97 <a_jb97@naver.com>
2026-02-01 23:17:43 +0900

    Feat : SwiftUI의 Charts를 이용해 ChartView 구현

---
+++ PhotoProject/Detail/ChartView.swift
@@ -0,0 +1,29 @@
+//
+//  ChartView.swift
+//  PhotoProject
+//
+//  Created by 전민돌 on 2/1/26.
+//
+
+import SwiftUI
+import Charts
+
+struct ChartView: View {
+    var viewsData: [ValuesDetail]
+    
+    var body: some View {
+        Chart {
+            ForEach(viewsData.indices, id: \.self) { index in
+                let data = viewsData[index]
+                let date = DateFormat.shared.makeStringToDateChart(data.date)
+                
+                LineMark(
+                    x: .value("Date", date),
+                    y: .value("Views", data.value)
+                )
+                .foregroundStyle(.blue)
+            }
+        }
+        .frame(height: 200)
+    }
+}
