3db79ba9b033715e7fc4253bbad327bdaf95131f
a-jb97 <a_jb97@naver.com>
2026-02-03 20:08:38 +0900

    Feat : 커스텀 뷰로 ProfileTextField 생성

---
+++ PhotoProject/CustomView/ProfileTextField.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileTextField.swift
+//  PhotoProject
+//
+//  Created by 전민돌 on 2/3/26.
+//
+
+import UIKit
+
+class ProfileTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(placeHolder: String) {
+        super.init(frame: .zero)
+        
+        clipsToBounds = true
+        layer.cornerRadius = 15
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.systemBlue.cgColor
+        placeholder = placeHolder
+    }
+}
