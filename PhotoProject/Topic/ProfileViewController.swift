12cb06eb44bfc67c97ff9ea772ac5ad36c59d132
a-jb97 <a_jb97@naver.com>
2026-02-03 21:27:21 +0900

    Feat : ProfileVC에서 확인버튼 선택했을 때 각 TextField에 대한 Error Handling 구현

--- PhotoProject/Topic/ProfileViewController.swift
+++ PhotoProject/Topic/ProfileViewController.swift
@@ -42,6 +42,14 @@ final class ProfileViewController: BaseViewController {
         
         return button
     }()
+    private let validationLabel = {
+        let label = UILabel()
+        
+        label.font = .boldSystemFont(ofSize: 14)
+        label.textAlignment = .center
+        
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,11 +58,54 @@ final class ProfileViewController: BaseViewController {
     }
     
     @objc private func checkButtonTapped() {
-        
+        do {
+            try validateTextFieldInput(
+                textList: [yearTextField.text!, monthTextField.text!, dayTextField.text!],
+                textFields: [yearTextField, monthTextField, dayTextField]
+            )
+            
+            validationLabel.text = "생년월일 기입이 성공했습니다."
+        } catch ValidationError.isNotNumber {
+            validationLabel.text = "숫자만 입력할 수 있습니다."
+        } catch ValidationError.notRangeMonth {
+            validationLabel.text = "'월'은 1 ~ 12 사이의 숫자만 입력할 수 있습니다."
+        } catch ValidationError.notRangeDay {
+            validationLabel.text = "'일'은1 ~ 31 사이의 숫자만 입력할 수 있습니다."
+        } catch ValidationError.containWhiteSpace {
+            validationLabel.text = "공백을 포함할 수 없습니다."
+        } catch {
+            validationLabel.text = ""
+        }
+    }
+    
+    private func validateTextFieldInput(textList: [String], textFields: [UITextField]) throws {
+        for i in 0..<textFields.count {
+            guard textList[i].contains(try Regex("[0-9]")) else {
+                throw ValidationError.isNotNumber
+            }
+            
+            if textFields[i] == monthTextField {
+                guard (1...12).contains(Int(textList[i].replacingOccurrences(of: " ", with: "")) ?? 0) else {
+                    throw ValidationError.notRangeMonth
+                }
+            }
+            
+            if textFields[i] == dayTextField {
+                guard (1...31).contains(Int(textList[i].replacingOccurrences(of: " ", with: "")) ?? 0) else {
+                    throw ValidationError.notRangeDay
+                }
+            }
+            
+            try textList[i].forEach { char in
+                if char.isWhitespace {
+                    throw ValidationError.containWhiteSpace
+                }
+            }
+        }
     }
     
     override func configureHierarchy() {
-        [yearTextField, monthTextField, dayTextField, checkButton].forEach { view.addSubview($0) }
+        [yearTextField, monthTextField, dayTextField, checkButton, validationLabel].forEach { view.addSubview($0) }
     }
     
     override func configureLayout() {
@@ -80,6 +131,11 @@ final class ProfileViewController: BaseViewController {
             make.top.equalTo(dayTextField.snp.bottom).offset(20)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(32)
             make.height.equalTo(56)
+        }
+        
+        validationLabel.snp.makeConstraints { make in
+            make.top.equalTo(checkButton.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(32)
         }
     }
 }
